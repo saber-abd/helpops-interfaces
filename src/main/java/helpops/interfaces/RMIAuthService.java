215f52b60f773196b3b313dfa1379ece1b7f9ca8
AminataAlidjiMaiga <aminatamaiag69@yahoo.com>
2026-03-10 15:19:11 +0100

    cleancode

--- src/main/java/helpops/interfaces/RMIAuthService.java
+++ src/main/java/helpops/interfaces/RMIAuthService.java
@@ -10,11 +10,11 @@ import java.util.UUID;
  * Gère le cycle de vie des utilisateurs, la validation des accès et le contrôle des rôles.
  */
 public interface RMIAuthService extends Remote {  //interface RMI pour authentification
-    Token connecter(String login, String motDePasse) throws RemoteException;  // return le token si reussi sinon null
-    boolean inscrire(String login, String motDePasse) throws RemoteException; // return true ou false selon le resultat, en cas de reusitte connecte directement l'utilisateur
-    boolean verifierToken(String tokenValeur) throws RemoteException; // return aussi true ou false selon la validite
+    Token connecter(String login, String motDePasse) throws RemoteException;  // retourne le token si reussi sinon null
+    boolean inscrire(String login, String motDePasse) throws RemoteException; // retourne true ou false selon le resultat, en cas de reusitte connecte directement l'utilisateur
+    boolean verifierToken(String tokenValeur) throws RemoteException; // retourne true ou false selon la validite
     UUID getUuidDepuisToken(String tokenValeur) throws RemoteException;
     String ping() throws RemoteException;  // test connexion RMI
     boolean changerRole(String tokenAgent, UUID utilisateurAChanger, String nouveauRole) throws RemoteException; // V2 : Administration des privilèges
-    String getRoleDepuisToken(String tokenValeur) throws RemoteException; // V2 : Validatio pour le serveur d'incident, ermet de vérifier à la fois si le token est bon ET quel est son rôle
+    String getRoleDepuisToken(String tokenValeur) throws RemoteException; // V2 : Validation pour le serveur d'incident, ermet de vérifier à la fois si le token est bon ET quel est son rôle
 }
